01128ab62d797df4ed5ae8da66214b9c864b5a0e
Rose <cinorflynn@yahoo.com>
2016-08-31 10:38:37 -0400

    Minor fix
    HAckneyed, Might break through prolonged use

--- Concerto/Assets/Rose Testing/Scripts/EnemyComboScript.cs
+++ Concerto/Assets/Rose Testing/Scripts/EnemyComboScript.cs
@@ -1,29 +1,52 @@
 ﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class EnemyComboScript : MonoBehaviour {
 
     public bool Selected = false;
     [SerializeField]
     GameObject TextGameObject;
+    [SerializeField]
     Queue<string> ComboLetters;
 	// Use this for initialization
 	void Start () {
+
+        //Puts in three standard buttons
         ComboLetters = new Queue<string>();
-        ComboLetters.Enqueue("space");
+        ComboLetters.Enqueue("a");
         ComboLetters.Enqueue("b");
         ComboLetters.Enqueue("c");
+
+
+        TextGameObject = gameObject.transform.Find("Enemy Letter").gameObject;
     }
 	
 	// Update is called once per frame
 	void Update () {
         TextGameObject.GetComponent<TextMesh>().text = ComboLetters.Peek();
-        if (Selected)
+        if (!Selected)
         {
-            if (Input.GetKeyDown(ComboLetters.Peek()) && ComboLetters.Count != 0 )
+            try
             {
-                string temp = ComboLetters.Dequeue();
-                Debug.Log("Dequeue'd " + temp);
+                if(ComboLetters.Peek() == "EndQueue")
+                {
+                    Destroy(this.gameObject);
+                }
+                if (Input.GetKeyDown(ComboLetters.Peek()))
+                {
+                    string temp = ComboLetters.Dequeue();
+                    Debug.Log("Dequeue'd " + temp);
+                    if(ComboLetters.Count == 1)
+                    {
+                        ComboLetters.Enqueue("EndQueue");
+                    }
+                }
+            }catch(InvalidOperationException e)
+            {
+                Debug.Log(e);
+                Destroy(this.gameObject);
             }
         }
 	}
