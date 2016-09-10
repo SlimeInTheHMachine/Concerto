1d4c66faa37f3c3d7ce456e3bf5fcad758dfc41b
Peter McNamara <pmm9335@rit.edu>
2016-09-10 14:30:55 -0400

    Tested having other objects subscribe to onBeat().

---
+++ Concerto/Assets/Compilation/TestSubscriber.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSubscriber : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start ()
+	{
+		//beatManager.GetComponent<BeatManager>().BeatFunction test = subscriberTest;
+		BeatManager.onBeat += subscriberTest;
+	}
+
+	void subscriberTest()
+	{
+		Debug.Log ("Entered subscriber function");
+	}
+}
