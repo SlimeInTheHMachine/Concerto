a0fe49df409b3df1659b2a74bea59c4a6616919f
Stefen Lewis (RIT Student) <sml5527@ad.rit.edu>
2016-09-19 19:42:43 -0400

    Level Finished

---
+++ Concerto/Assets/Compilation/Scripts/PlatMoverHorizantal.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatMoverHorizantal : MonoBehaviour {
+    bool moveForward;
+    public float right;
+    GameObject beatManager;
+
+    private float startTime;
+    private float timeBetweenPosChange;
+    private double beatTime;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+        beatManager = GameObject.Find("BeatManager");
+        beatTime = beatManager.GetComponent<BeatManager>().BeatTime;
+        startTime = Time.time;
+        timeBetweenPosChange = (float)beatTime;
+        moveForward = true;
+        startPos = transform.position;
+        endPos = new Vector3(startPos.x + right, startPos.y);
+    }
+
+    void FixedUpdate()
+    {
+        if (moveForward)
+        {
+
+            float PosChanged = (Time.time - startTime) * Mathf.Abs(transform.position.x) / timeBetweenPosChange;
+            float t = PosChanged / Mathf.Abs(endPos.x);
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            if (transform.position.x == endPos.x)
+            {
+                moveForward = false;
+                startTime = Time.time;
+            }
+        }
+        else
+        {
+            float PosChanged = (Time.time - startTime) * Mathf.Abs(transform.position.x) / timeBetweenPosChange;
+            float t = PosChanged / Mathf.Abs(startPos.x);
+            transform.position = Vector2.Lerp(endPos, startPos, t);
+
+            if (transform.position.x == startPos.x )
+            {
+                moveForward = true;
+                startTime = Time.time;
+            }
+        }
+    }
+}
---
+++ Concerto/Assets/Compilation/Scripts/PlatMoverVertical.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatMoverVertical : MonoBehaviour
+{
+    bool moveForward;
+    public float up;
+    GameObject beatManager;
+
+    private float startTime;
+    private float timeBetweenPosChange;
+    private double beatTime;
+    private Vector3 startPos;
+    private Vector3 endPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        beatManager = GameObject.Find("BeatManager");
+        beatTime = beatManager.GetComponent<BeatManager>().BeatTime;
+        startTime = Time.time;
+        timeBetweenPosChange = (float)beatTime;
+        moveForward = true;
+        startPos = transform.position;
+        endPos = new Vector3(startPos.x , startPos.y + up);
+    }
+
+    void FixedUpdate()
+    {
+        if (moveForward)
+        {
+
+            float PosChanged = (Time.time - startTime) * Mathf.Abs(transform.position.y) / timeBetweenPosChange;
+            float t = PosChanged / Mathf.Abs(endPos.y);
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            if (transform.position.y == endPos.y)
+            {
+                moveForward = false;
+                startTime = Time.time;
+            }
+        }
+        else
+        {
+            float PosChanged = (Time.time - startTime) * Mathf.Abs(transform.position.y) / timeBetweenPosChange;
+            float t = PosChanged / Mathf.Abs(startPos.y);
+            transform.position = Vector2.Lerp(endPos, startPos, t);
+
+            if (transform.position.y == startPos.y)
+            {
+                moveForward = true;
+                startTime = Time.time;
+            }
+        }
+    }
+}
