69dd4626b23377cc21ae9644e7c8b9cbba57da92
Blane Hadley (RIT Student) <bfh1438@ad.rit.edu>
2016-10-26 15:42:47 -0400

    Previous Commit in rush
    Previous Commit:
    Changed square sprite
    Changed cam bob
    Deactivated background
    Edited player values
    edited player collider values
    
    This commit:
    created AudioMan script and audioManager obj
    Added beat audio

---
+++ Concerto/Assets/Scripts/Audio/AudioMan.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+
+
+public class AudioMan : MonoBehaviour
+{
+    private AudioSource audioSrc;
+    // Use this for initialization
+    void Start () {
+        audioSrc = transform.GetComponent<AudioSource>();
+        BeatMan.startBeat += PlayAudio;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PlayAudio()
+    {
+        audioSrc.Play();
+    }
+}
