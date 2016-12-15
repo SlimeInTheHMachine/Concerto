8ba51740341c293d0577266f37beee852c6d3c9c
Rose <cinorflynn@yahoo.com>
2016-12-15 11:47:14 -0500

    Rose makes a new Enemy. A S D are Hi Med and Low hits on the enemy for xombos.

--- Concerto/Assets/Scripts/Player/Platformer.cs
+++ Concerto/Assets/Scripts/Player/Platformer.cs
@@ -26,7 +26,7 @@ public class Platformer : MonoBehaviour {
     private float shake;
 
     private attackInputs attackInput;
-    private ComboScript currentEnemy;
+    private Combo2   currentEnemy;
 
     private bool haveAttacked;
     private bool haveMoved;
@@ -62,6 +62,8 @@ public class Platformer : MonoBehaviour {
         set { canFall = value; }
     }
 
+    [SerializeField]
+    GameObject fightSprite, LeftFist, RightFist;
     //Called before all start functions
     void Awake()
     {
@@ -267,9 +269,10 @@ public class Platformer : MonoBehaviour {
         }
 
         //Attack
-        if (enemyRayHit.collider != null && (Input.GetButtonDown("AButton") || Input.GetButtonDown("BButton") || Input.GetButtonDown("XButton") || Input.GetButtonDown("YButton")))
+        if (enemyRayHit.collider != null)
         {
-                currentEnemy = enemyRayHit.transform.gameObject.GetComponent<ComboScript>();
+            Debug.Log("I see an enemy");
+                currentEnemy = enemyRayHit.transform.gameObject.GetComponent<Combo2>();
                 CombatInput();
         }
 
@@ -299,52 +302,48 @@ public class Platformer : MonoBehaviour {
     /// </summary>
     void CombatInput()
     {
+        //Hits hit;
         //See if there is combat input
         //If multiple inputs, Garbage Input
-        if (Input.GetButtonDown("AButton"))
-        {
-            if (attackInput == attackInputs.None)
-                attackInput = attackInputs.A;
-            else
-                attackInput = attackInputs.Garbage;
+         if (UnityEngine.Input.GetButtonDown("XButton"))
+         {
+            currentEnemy.GetComponent<Combo2>().HitHigh(Hits.high);
+            return;
+         }
+         if (UnityEngine.Input.GetButtonDown("YButton"))
+         {
+            currentEnemy.GetComponent<Combo2>().HitMed(Hits.med);
+            return;
         }
-        if (Input.GetButtonDown("BButton"))
-        {
-            if (attackInput == attackInputs.None)
-                attackInput = attackInputs.B;
-            else
-                attackInput = attackInputs.Garbage;
-        }
-        if (Input.GetButtonDown("XButton"))
-        {
-            if (attackInput == attackInputs.None)
-                attackInput = attackInputs.X;
-            else
-                attackInput = attackInputs.Garbage;
-        }
-        if (Input.GetButtonDown("YButton"))
-        {
-            if (attackInput == attackInputs.None)
-                attackInput = attackInputs.Y;
-            else
-                attackInput = attackInputs.Garbage;
-        }
+         if (UnityEngine.Input.GetButtonDown("BButton"))
+         {
+            currentEnemy.GetComponent<Combo2>().HitLow(Hits.low);
+            return;
+         }
+        
 
-        //If Offbeat or button mash, Garbage Input
-        if (!BeatMan.instance.onTime || haveAttacked)
-            attackInput = attackInputs.Garbage;
-
-        //Clash back if you mess up
-        if(!currentEnemy.CheckInput(attackInput))
-        {
-            //clash code here
-            Clash();
-            currentEnemy = null;
-        }
-        //We've attempted to attack this beat
-        haveAttacked = true;    
     }
+    public void GenerateRight(float updwn)
+    {
+        GameObject temp = (GameObject)Instantiate(fightSprite);
+        Vector3 tempP = new Vector3(RightFist.transform.position.x, RightFist.transform.position.y + updwn,RightFist.transform.position.z);//)
+        temp.transform.position = tempP;
+        Destroy(temp, 0.5f);
+    }
+    public void GenerateLeft()
+    {
+        GameObject temp = (GameObject)Instantiate(fightSprite);
 
+        temp.transform.position = LeftFist.transform.position;
+        Destroy(temp, 0.5f);
+    }
+    void GenerateFightSprite()
+    {
+        GameObject temp = (GameObject)Instantiate(fightSprite);
+        
+        temp.transform.position = transform.position ;
+        Destroy(temp, 0.5f);
+    }
     /// <summary>
     /// Bounce away from enemy
     /// </summary>
@@ -365,11 +364,11 @@ public class Platformer : MonoBehaviour {
         if (currentEnemy != null && !haveAttacked)
         {
             attackInput = attackInputs.None;
-            if (!currentEnemy.CheckInput(attackInput))
-            {
-                Clash();
-                currentEnemy = null;
-            }
+            //if (!currentEnemy.CheckInput(attackInput))
+            //{
+            //   // Clash();
+            //    currentEnemy = null;
+            //}
         }
         haveAttacked = true;
 
